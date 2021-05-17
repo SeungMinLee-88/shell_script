db9f4118a16840f96545983e0a0b7d686bbc688d
LEESeungMin88 <rogirl711@gmail.com>
2021-05-17 14:47:23 +0900

    shell_script

--- bak/makezone.sh
+++
@@ -1,6 +0,0 @@
-zonename=
-    echo "12312"
-zonename=$(dialog --stdout --title "My input box" --clear \
-        --inputbox "Hi, this is a sample input box\n Try entering your name below:" \
-        16 51 2)
-echo $zonename
--- bak/require.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-while true
-do
-echo "require----------------------111111"$(date)
-sleep 2
-echo "require----------------------222222"$(date)
-done
